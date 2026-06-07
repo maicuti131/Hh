45e2ab3e000f91849ec5e977e63c48080bd0a39a
maicuti131 <ng.thanhthao0207e@gmail.com>
2026-06-07 13:14:21 +0700

    Create server.js

---
+++ chat-ai/server/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Chat AI API Running"
+  });
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/chat", require("./routes/chat"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
